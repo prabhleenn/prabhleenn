817f3716d50327947e43deae6fc24ad79d122f0e
Prabhleen Kaur <114502930+prabhleenn@users.noreply.github.com>
2023-08-11 23:38:27 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        //Write your code here
+        
+        int n; 
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+    
+        if (n<0){
+            System.out.println("Negative");
+            }
+            else {
+                System.out.println("Positive");
+            }
+     
+       }
+}
