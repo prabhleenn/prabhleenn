7fe0abcd99ab65935ad89236a47b829069ebe4ba
Prabhleen Kaur <114502930+prabhleenn@users.noreply.github.com>
2023-08-11 23:39:19 +0530

    Add files via upload

---
+++ MultiplyFloatingNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        // Write your code here
+    int F1, F2;
+    Scanner s = new Scanner(System.in);
+
+    F1 = s.nextInt();
+    F2 = s.nextInt();
+
+    long sum =(long) F1*F2;
+    System.out.println(sum);
+
+    }
+}
