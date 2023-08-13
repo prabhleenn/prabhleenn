bf8819b35fa272f1fd970fb239e06daa1f87c058
Prabhleen Kaur <114502930+prabhleenn@users.noreply.github.com>
2023-08-13 23:08:04 +0530

    Delete PrintASCIIvalue.java

--- PrintASCIIvalue.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char c = sc.next().charAt(0);
-        System.out.print((int)c);
-    }
-}
